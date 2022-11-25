5edc821407aec52d8ac302e5bfa5c9c9bf398020
tanghongzhong <729107180@qq.com>
2022-11-26 00:25:46 +0800

    test

---
+++ src/components/xunkuButton/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button>测试按钮</button>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'xunkuButton',
+  }
+  </script>
+  
+  <style lang="scss">
+  </style>
+  
